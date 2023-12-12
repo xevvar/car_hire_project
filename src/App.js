1840dd2f0e819d8081d0f9adb129138168301b9c
anthony <gachinigaithuma@gmail.com>
2023-12-12 15:39:28 +0300

    done testing branches

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          view branch change
+          Learn React
         </a>
       </header>
     </div>
