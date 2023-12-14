7cde7f2109edac3e2692c60fcb1c1bff6dc256f5
btsalwa <brian.tsalwa@student.moringaschool.com>
2023-12-14 08:15:43 +0300

    Navbar folder with nav.js and css. Login folder with login and register form in react route and its css. App.js has the routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from "../src/Navbar/Navbar"
+import { Login  } from './Login/Login';
+import {Register} from './Login/Register';
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/login">
+          <Login onFormSwitch={toggleForm} />
+        </Route>
+        <Route path="/register">
+          <Register onFormSwitch={toggleForm} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
