6085bdbbf0d896e77d5c9fac37701bab66733398
anthony <gachinigaithuma@gmail.com>
2023-12-17 22:38:16 +0300

    displayed list

--- src/Components/CarList.js
+++ src/Components/CarList.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function CarList() {
-  return (
-    <div>
-        <h2>Our Cars</h2>
+
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/cars')
+          .then(response => setCars(response.data))
+          .catch(error => console.error('Error fetching data:', error));
+      }, []);
+
+    return (
+        <div>
+            <h2>Our Cars</h2>
+            <ul>
+                {cars.map((car) => (
+            <li key={car.id}>
+                <h4> {car.carName}</h4>
+                <img src={car.imgUrl} alt={`${car.carName}`}/>
+
+            </li>
+        ))}
+            </ul>
+
+
         </div>
-  )
+    )
 }
 
 export default CarList
